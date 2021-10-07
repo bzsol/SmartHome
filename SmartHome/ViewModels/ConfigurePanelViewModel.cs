28a12f1c8ec7067e37d7571d0b13b87144627c05
Koszta <koszta002@gmail.com>
2021-10-07 15:43:21 +0200

    Prism.core integration

--- SmartHome/ViewModels/ConfigurePanelViewModel.cs
+++ SmartHome/ViewModels/ConfigurePanelViewModel.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SmartHome.ViewModels
 {
     public class ConfigurePanelViewModel
     {
+        public ConfigurePanelViewModel()
+        {
+
+        }
     }
 }
