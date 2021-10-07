5981a3a6a773b4bdb2eda988bd690eef46c8d11f
Koszta <koszta002@gmail.com>
2021-10-07 14:33:36 +0200

    update baseui

--- SmartHome/Views/ConfigurePanel.xaml.cs
+++ SmartHome/Views/ConfigurePanel.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SmartHome
+namespace SmartHome.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ConfigurePanel.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ConfigurePanel : Window
     {
-        public MainWindow()
+        public ConfigurePanel()
         {
             InitializeComponent();
         }
