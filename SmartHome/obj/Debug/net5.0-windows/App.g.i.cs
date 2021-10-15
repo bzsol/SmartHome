08076c20f6abae26f11fbc7cbd3ec978d3ebef1b
filepfannikiara <filepfannikiara@gmail.com>
2021-10-15 15:25:03 +0200

    git merge

--- SmartHome/obj/Debug/net5.0-windows/App.g.i.cs
+++ SmartHome/obj/Debug/net5.0-windows/App.g.i.cs
@@ -49,7 +49,7 @@ namespace SmartHome {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -61,7 +61,7 @@ namespace SmartHome {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/SmartHome;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SmartHome;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -75,7 +75,7 @@ namespace SmartHome {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.2.0")]
         public static void Main() {
             SmartHome.App app = new SmartHome.App();
             app.InitializeComponent();
