8972907c467747cb8c0d5c3148eb5a934a7c8d43
bzsol <bereczzsolt2000@gmail.com>
2021-11-21 16:41:19 +0100

    Added Timer to the UI and the system

--- SmartHome/ViewModels/DashboardViewModel.cs
+++ SmartHome/ViewModels/DashboardViewModel.cs
@@ -1,9 +1,14 @@
-﻿using Prism.Commands;
+﻿using Common.Class;
+using Common.Model;
+using Prism.Commands;
+using SmartHome.DataProvider;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,8 +16,10 @@ using System.Windows.Threading;
 
 namespace SmartHome.ViewModels
 {
+
     public class DashboardViewModel : INotifyPropertyChanged
     {
+        public int time = 0;
         public DelegateCommand<Button> ChangeToSimulation { get; set; }
         public DelegateCommand<Button> ChangeToConfiguration { get; set; }
 
@@ -27,6 +34,15 @@ namespace SmartHome.ViewModels
             }
         }
 
+        private string _timechange;
+        public string TimeChange {
+            get => _timechange;
+            set {
+                _timechange = value;
+                NotifyChange(nameof(TimeChange));
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void NotifyChange(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
@@ -35,6 +51,29 @@ namespace SmartHome.ViewModels
         {
             ChangeToSimulation = new DelegateCommand<Button>(OnChangeToSimulation);
             ChangeToConfiguration = new DelegateCommand<Button>(OnChangeToConfiguration);
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("hu-HU");
+            if (((List<ExternalFactors>)ExtFactDataProvider.Get()).Count < 1)
+            {
+                // Generate new list without null
+                ExtFactDataProvider.Create(new ExternalFactors(new List<Electronics>(), new List<Electronics>(), new Climate(), new Climate(), new Climate(), new Climate(), new Climate(), new Climate(), new Climate(), new Climate(), new Climate(), new Lights(), new Lights(), new Lights(),
+                    new Lights(), new Lights(), new Lights(), new Lights(), new Lights(), new Lights(), new Lights(),
+                    new Lights(), new Lights(), new Lights(), new Irrigative(), new Irrigative(), new Shading(), new Shading(), new Shading(), new Shading(), new Shading(), new Shading(), new Shading(), new Shading(), new Shading(), new Shading(), new Shading()));
+            }
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            dispatcherTimer.Start();
+
+        }
+        void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            time++;
+            TimeChange = SecToMilitaryTime(time);
+        }
+
+        private string SecToMilitaryTime(int seconds) {
+            TimeSpan time = TimeSpan.FromSeconds(seconds);
+            return time.ToString(@"hh\:mm\:ss");
         }
 
         public void OnChangeToSimulation(Button btn)
--- SmartHome/Views/SimulationPanel.xaml.cs
+++ SmartHome/Views/SimulationPanel.xaml.cs
@@ -27,20 +27,11 @@ namespace SmartHome.Views
 
     public partial class SimulationPanel : UserControl
     {
-        private int asd = 0;
         public SimulationPanel()
         {
             InitializeComponent();
-            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
-            dispatcherTimer.Tick += dispatcherTimer_Tick;
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
-            dispatcherTimer.Start();
             
 
-        }
-        void dispatcherTimer_Tick(object sender, EventArgs e)
-        {
-            asd++;
         }
     }
             
