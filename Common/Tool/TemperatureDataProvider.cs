fef9e0104ac5b33651a95a282eff2a341bae7ac1
bzsol <bereczzsolt2000@gmail.com>
2021-11-24 13:19:24 +0100

    comment

--- Common/Tool/TemperatureDataProvider.cs
+++ Common/Tool/TemperatureDataProvider.cs
@@ -9,7 +9,7 @@ namespace Common.Tool
     public static class TemperatureDataProvider
     {
         public static double GenerateTemp(int t) {
-
+            // Kilengés * Sin(hossz*(t-eltolás X)) + y eltolás
             return 9 * Math.Sin(0.3 * (t - 6.5)) + 13;
         }
         
